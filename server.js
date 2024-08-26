280877043a46f025ad9bf9026a56b18037e34d1c
Gustavo361 <gustavofaustino361@gmail.com>
2024-08-26 03:40:20 -0300

    insert data into MySql database

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ connection.connect((error) => {
     if (error) {
         console.log('connection error:', error)
     } else {
-        console.log('connection established')
+        console.log('successfully connected to MySQL database!')
     }
 })
 
@@ -31,11 +31,21 @@ app.post('/submit', (req, res) => {
     let name = req.body.name
     let age = req.body.age
     let gender = req.body.gender
+    
+    console.log('received form data - Name:', name, ', Age:', age, ', Gender:', gender)
 
-    console.log('dados recebidos:', name, age, gender)
+    const sql = 'INSERT INTO simple_users (user_name, user_age, user_gender) VALUES (?,?,?)'
 
-    // res.redirect(`/received?name=${name}&age=${age}&gender=${gender}`);
-    res.redirect(`/received?name=${encodeURIComponent(name)}&age=${encodeURIComponent(age)}&gender=${encodeURIComponent(gender)}`)
+    connection.query(sql, [req.body.name, req.body.age, req.body.gender], (error, results) => {
+        if (error) {
+            console.error('Error inserting data', error)
+            res.status(500).send('Error saveing data')
+            return
+        }
+        console.log('success saving data:', results)
+        // res.redirect(`/received?name=${name}&age=${age}&gender=${gender}`);
+        res.redirect(`/received?name=${encodeURIComponent(name)}&age=${encodeURIComponent(age)}&gender=${encodeURIComponent(gender)}`)
+    })
 })
 
 app.get('/received', (req, res) => {
