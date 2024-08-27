9986dcded91a8714a1e4d5d5cac08d31d5ed9b16
Gustavo361 <gustavofaustino361@gmail.com>
2024-08-27 02:00:02 -0300

    getting all and last records from MySQL db

--- server.js
+++ server.js
@@ -31,19 +31,17 @@ app.post('/submit', (req, res) => {
     let name = req.body.name
     let age = req.body.age
     let gender = req.body.gender
-    
-    console.log('received form data - Name:', name, ', Age:', age, ', Gender:', gender)
 
     const sql = 'INSERT INTO simple_users (user_name, user_age, user_gender) VALUES (?,?,?)'
 
     connection.query(sql, [req.body.name, req.body.age, req.body.gender], (error, results) => {
         if (error) {
-            console.error('Error inserting data', error)
-            res.status(500).send('Error saveing data')
+            console.error('error inserting data', error)
+            res.status(500).send('Error saving data')
             return
         }
         console.log('success saving data:', results)
-        // res.redirect(`/received?name=${name}&age=${age}&gender=${gender}`);
+        // res.redirect(`/received?name=${name}&age=${age}&gender=${gender}`)
         res.redirect(`/received?name=${encodeURIComponent(name)}&age=${encodeURIComponent(age)}&gender=${encodeURIComponent(gender)}`)
     })
 })
@@ -52,6 +50,32 @@ app.get('/received', (req, res) => {
     res.sendFile(path.join(__dirname, 'received.html'))
 })
 
+app.get('/show-last-user', (req, res) => {
+    const sql = 'SELECT * FROM simple_users ORDER BY id DESC LIMIT 1'
+
+    connection.query(sql, (error, results)=>{
+        if (error) {
+            console.log('error getting last record', error)
+        } else {
+            res.send(results)
+        }
+    })
+})
+
+app.get('/show-all', (req, res) => {
+    console.log('entrou no get show-all')
+    const sql = 'SELECT * FROM simple_users'
+
+    connection.query(sql, (error, results)=>{
+        if (error) {
+            console.error('error getting all data from MySQL database', error)
+        } else {
+            console.log('response: ', results)
+            res.send(results)
+        }
+    })
+})
+
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
-});
+    console.log('server is running on http://localhost:3000')
+})
