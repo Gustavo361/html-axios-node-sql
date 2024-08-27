eed6900861e07b26c5860a6fe2ff111e1014b9b0
Gustavo361 <gustavofaustino361@gmail.com>
2024-08-27 02:04:19 -0300

    request all or last records with axios

--- received.js
+++ received.js
@@ -1,18 +1,62 @@
-// recieving data in frontend
+const showAll = document.getElementById('show-all')
+const showLastRecord = document.getElementById('show-last-record')
+const dataBase = document.getElementById('database')
+const btnClearQuery = document.getElementById('clear-query')
 
-function getQueryParams() {
-    const params = new URLSearchParams(window.location.search)
-    const data = {}
-    for (const [key, value] of params.entries()) {
-        data[key] = value
-    }
-    return data
+showAll.addEventListener('click', () => {
+    clearQuery()
+    axios.get('/show-all')
+        .then(response => {
+            createTable(response.data)
+        })
+        .catch(error => {
+            console.error('Erro na requisição GET com axios', error)
+        })
+})
+
+showLastRecord.addEventListener('click', () => {
+    clearQuery()
+    axios.get('/show-last-user')
+        .then(response => {
+            createTable(response.data)
+        })
+        .catch(error => {
+            console.log('error getting last record:', error)
+        })
+})
+
+btnClearQuery.addEventListener('click', () => {
+    clearQuery()
+})
+
+function clearQuery() {
+    dataBase.innerHTML = ''
+    table = document.createElement('table')
+    tableRow = document.createElement('tr')
 }
 
-const data = getQueryParams()
-document.getElementById('data').innerHTML = `
-                                            DATA <br>
-                                            <span>Name: ${data.name}</span><br>
-                                            <span>Age: ${data.age}</span><br>
-                                            <span>Gender: ${data.gender}</span>
-                                             `
+function createTable(responseData) {
+    let table = document.createElement('table')
+    let tableRow = document.createElement('tr')
+
+    Object.keys(responseData[0]).forEach(key => {
+        const th = document.createElement('th')
+        th.textContent = key
+        tableRow.appendChild(th)
+    })
+
+    table.appendChild(tableRow)
+
+    responseData.forEach(item => {
+        const row = document.createElement('tr')
+
+        Object.values(item).forEach(value => {
+            const td = document.createElement('td')
+            td.textContent = value
+            row.appendChild(td)
+        })
+        table.appendChild(row)
+    })
+
+    dataBase.appendChild(table)
+}
